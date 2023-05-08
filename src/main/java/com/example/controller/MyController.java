5d715d10cccd55f7acb5f40c5eec0cfb694ce2ea
fqq <2998657166@qq.com>
2023-05-08 15:29:04 +0800

    提交了更改

--- src/main/java/com/example/controller/MyController.java
+++ src/main/java/com/example/controller/MyController.java
@@ -1,15 +1,19 @@
 package com.example.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 @RequestMapping("/example")
 public class MyController {
-    @GetMapping("/welcome")
-    public ResponseEntity<String> welcome() {
+    @PostMapping("/message")
+    public ResponseEntity<String> message(@RequestBody Map<String, Object> message) {
+        System.out.println("receive message: " + message);
         String welcome = "welcome for spring boot with web";
         System.out.println(welcome);
         return ResponseEntity.ok(welcome);
